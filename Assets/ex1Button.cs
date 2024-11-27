e0d42bde7e122cc4f4d95a64924224710815463e
evian-christ <58662680+evian-christ@users.noreply.github.com>
2024-11-27 15:33:38 +0000

    button text switch loop exercise completed

---
+++ Assets/ex1Button.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public class ex1Button : MonoBehaviour
+{
+    public TextMeshProUGUI buttonText;
+    int counter = 0;
+
+    public void buttonTextChange()
+    {
+        switch (counter)
+        {
+            case 0:
+                buttonText.text = "Play";
+                break;
+            case 1:
+                buttonText.text = "Restart";
+                break;
+            case 2:
+                buttonText.text = "Quit";
+                break;
+            case 3:
+                buttonText.text = "Start";
+                break;
+
+        }
+        if (counter < 3)
+        {
+            counter++;
+        }
+        else
+        {
+            counter = 0;
+        }
+    }
+}
